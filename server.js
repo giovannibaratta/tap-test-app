a8bfb8614b20b404bdf90ab8239d13c4c69b4211
Giovanni Baratta <bgiovanni@vmware.com>
2023-12-20 16:15:01 +0100

    new commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server. This is a new commit!');
+  res.send('Hello from the server. This is a new commit!!');
 });
 
 app.get('/health', (req, res) => {
