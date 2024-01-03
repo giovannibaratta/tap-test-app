11a6f9e0cb64bb176baade3e64ab203af5189b3b
Giovanni Baratta <bgiovanni@vmware.com>
2024-01-03 12:35:29 +0100

    a new message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("A new message11");
+  res.send("A new message111");
 });
 
 app.get("/health", (req, res) => {
