0a5a8f7af29766f5f3bd61f23c06d7d287aa750b
Giovanni Baratta <bgiovanni@vmware.com>
2024-01-03 15:45:07 +0100

    Add apis

--- server.js
+++ server.js
@@ -10,6 +10,37 @@ app.get("/health", (req, res) => {
   res.sendStatus(200);
 });
 
+const apis = {
+  "openapi": "3.0.2",
+  "info": {
+    "version": "1.0.0",
+    "title": "test"
+  },
+  "paths": {
+    "/health": {
+      "get": {
+        "operationId": "getHealth",
+        "responses": {
+          "200": {
+            "description": "Successful response",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+app.get("/api", (req, res) => {
+  res.json(apis)
+})
+
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
